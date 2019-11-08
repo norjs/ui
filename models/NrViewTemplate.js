2e1dea95057a3b95b4247175a42249d8c0efe4d3
Jaakko Heusala <jheusala@iki.fi>
2019-11-08 14:07:50 +0200

    Updated JSDocs

--- models/NrViewTemplate.js
+++ models/NrViewTemplate.js
@@ -3,15 +3,34 @@
  *
  * This is used in the NorJS Edge Backend.
  *
+ * Each session usually will have its own view template.
+ *
  * @interface
  */
 export class NrViewTemplate {
 
+    /**
+     * Initializes the view template after first request to new session or each time for non-session requests.
+     *
+     * This is called once after NorJS has initialized the template using component's bindings configuration.
+     *
+     * It can return a promise, in which case the promise will be waited until the request is done.
+     */
     init () {}
 
+    /**
+     * Destroys the view template when session is destroyed, or for non-session requests each time the request ends.
+     *
+     * You should delete any references to outside objects here.
+     *
+     * You may start asynchronic operations, but you should not return a promise. Deletion should be instant.
+     *
+     * The same object should not be used after it has been destroyed.
+     */
     destroy () {}
 
     /**
+     * Renders the view. This is called once for each request.
      *
      * @returns {NrModel}
      */
