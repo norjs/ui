8ecf1bdea2989c9754c04aa019092907c4457fb6
Jaakko Heusala <jheusala@iki.fi>
2019-11-13 10:00:08 +0200

    Added id and version to NrUser

--- models/NrUser.js
+++ models/NrUser.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import "./NrModel";
 import NrObjectType from "./NrObjectType";
+import LogUtils from "@norjs/utils/Log";
 
 /**
  *
@@ -35,15 +36,42 @@ export class NrUser {
 
     /**
      *
+     * @param [id] {string}
+     * @param [version] {number}
      * @param [email] {string}
+     * @param [name] {string}
      */
     constructor ({
+        id = undefined,
+        version = 0,
         email = undefined
     } = {}) {
+
+        if ( id !== undefined && !(id && _.isString(id)) ) {
+            throw new TypeError(`new ${NrUser.nrName}(): id invalid: ${LogUtils.getAsString(id)}`);
+        }
+
+        if ( !_.isNumber(version) ) {
+            throw new TypeError(`new ${NrUser.nrName}(): version invalid: ${LogUtils.getAsString(version)}`);
+        }
 
         if ( email !== undefined && !_.isString(email) ) {
             throw new TypeError(`new ${NrUser.nrName}(): email invalid: "${email}"`);
         }
+
+        /**
+         *
+         * @member {string}
+         * @protected
+         */
+        this._id = id;
+
+        /**
+         *
+         * @member {number}
+         * @protected
+         */
+        this._version = version;
 
         /**
          *
@@ -64,6 +92,23 @@ export class NrUser {
 
     /**
      *
+     *
+     * @returns {string|undefined}
+     */
+    get id () {
+        return this._id;
+    }
+
+    /**
+     *
+     * @returns {number|undefined}
+     */
+    get version () {
+        return this._version;
+    }
+
+    /**
+     *
      * @returns {string}
      */
     get email () {
@@ -77,6 +122,8 @@ export class NrUser {
     valueOf () {
         return {
             type: this.type,
+            id: this._id,
+            version: this._version,
             email: this._email ? this._email : null
         };
     }
@@ -108,10 +155,16 @@ export class NrUser {
             throw new TypeError(`${this.nrName}.parseValue(): value's type is not correct: "${value.type}"`);
         }
 
-        const { email } = value;
+        const {
+            id,
+            version,
+            email
+        } = value;
 
         return new NrUser({
-            email: !_.isNil(email) ? email : undefined
+            id      : !_.isNil(id)    ? id    : undefined,
+            version : version,
+            email   : !_.isNil(email) ? email : undefined
         });
 
     }
